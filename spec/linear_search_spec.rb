8c822a02fb34565c16216fdd410ac29e57eb275e
7minutech <elijohnson1107@gmail.com>
2024-09-01 20:43:20 -0500

    feat: Added test when value isn't in the array
    If the value isn't in the array the return value
    should be nil

--- spec/linear_search_spec.rb
+++ spec/linear_search_spec.rb
@@ -4,4 +4,7 @@ describe "#search" do
   it "returns the value of the target in the array" do
     expect(search([1,2,3,4,5],5)).to eql(5)
   end
+  it "returns the value nil if value isn't in array" do
+    expect(search([1,2,3,4],5)).to eql(nil)
+  end
 end
